72173b1f08794e53996a95f3693658a0f3a3f9d0
John Mitysumy <johnmitysumy@gmail.com>
2022-01-25 09:31:11 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
+  <link rel="icon" href="Daco_11030.png">
+</head>
+<title>
+	CHAMA WHATS
+</title>
+
+<meta name="keywords" content="gerador de link whatsapp" />
+<meta name="keywords" content="gerar link do whatsapp" />
+<meta name="keywords" content="chamar alguém no whatsapp sem salvar o contato" />
+
+<body style="	background-image: linear-gradient(to bottom, rgba(10, 0, 0, 0.1), rgba(100, 100, 100, 0.10)), url(&quot;wp7563284.jpg&quot;);	background-size: 300%, 250%;	background-repeat: repeat, repeat;" >
+  <div class="py-5 text-center align-items-center d-flex" style="" >
+    <div class="container py-3">
+      <div class="row">
+        <div class="col-md-8 mx-auto"> <img class="img-fluid d-block rounded-circle w-25 mx-auto" src="Daco_11030.png">
+          <h1 class="display-3 mb-4">CHAMA NO WHATS</h1>
+          <p class="lead mb-5 text-light">Chame pessoas no whatsapp sem precisar adicionar contato na agenda e ficar lotando sua agenda com pessoas que você só vai falar uma vez. <br>Digite o DDD + Número do usuario que você quer chamar e vá direto para o whatsapp.<br></p>
+          <form action="link.php"  method="post" class="form-inline text-center" style="">
+            <div class="input-group w-100 text-center">
+			
+              <input type="number" name=nome class="form-control" target="_blank" placeholder="DDD + Número">
+              <div class="input-group-append"><input lass="btn btn-primary" value="CHAMAR NO WHATS" type=submit class="btn btn-success"></div>
+			  
+            </div>
+          </form>
+		  <br>
+		  
+		  <p class="lead mb-5 text-light">
+		  Quantidade de Visitas: 538</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  
+</body>
+
+</html> 
+<a href="#" id="foo"></a>
+<center>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- BLOCO RESPONSIVO WHAT -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5995190762196084"
+     data-ad-slot="6026227739"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+<script>
+window.location.href='#ancora';
+</script>
+<a href="#" id="ancora">
+</center>
